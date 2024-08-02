6057658447d5a2927e39a8db464aff6f9a97ff31
3XV0T3R <3xv0t3r@gmail.com>
2024-08-02 15:21:25 -0300

    camphish.sh

---
+++ camphish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# CamPhish Installer Script
+
+echo "----------------------------------------"
+echo "   Bem-vindo ao instalador do CamPhish!"
+echo "----------------------------------------"
+
+# Exibindo informações sobre o script
+echo "O CamPhish é uma ferramenta para capturar imagens da câmera frontal do dispositivo alvo."
+echo "Para obter acesso completo à ferramenta e receber suporte, você precisará entrar em contato com o 3XV0T3R."
+
+# Instruções de contato
+echo "Para obter acesso à ferramenta, siga os seguintes passos:"
+echo "1. Entre no servidor oficial do Discord: https://discord.gg/9MBRne88mY"
+echo "2. Entre em contato com o usuário 3XV0T3R para solicitar acesso."
+
+# Exibindo uma mensagem final
+echo "A instalação está concluída. Se precisar de assistência, não hesite em buscar ajuda no servidor Discord."
+
+# Instruções para o usuário
+echo "--------------------------------------------------------"
+echo "Para começar a utilizar a ferramenta, siga as instruções fornecidas no repositório GitHub."
+echo "--------------------------------------------------------"
+
+# Finalizando
+exit 0
